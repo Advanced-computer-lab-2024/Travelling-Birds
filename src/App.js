8359c6ba2b4b51aa7b13c283b58d17607a176ebc
AlySerry <alyserry@outlook.com>
2024-09-29 02:21:57 +0300

    Initialize Node.js and object Schemas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
+    const Click = () => {
+        console.log('Button clicked');
+    }
   return (
     <div className="bold">
         <h1>Hello, world!</h1>
+        <button onClick={Click}>Click me</button>
     </div>
   );
 }
