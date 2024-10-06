06b46ca353d0064a539931e1ae396eb0a1431be9
AlySerry <alyserry@outlook.com>
2024-10-06 12:50:15 +0300

    Routing
    Signed-off-by: AlySerry <alyserry@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,39 +1,7 @@
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
-import RegisterPage from "./Pages/RegisterPage";
-import {TouristForm} from "./Components/Registration Forms/TouristForm";
-import {TourGuideForm} from "./Components/Registration Forms/TourGuideForm";
-import {SellerForm} from "./Components/Registration Forms/SellerForm";
-import {AdminForm} from "./Components/Registration Forms/AdminForm";
-import {AdvertiserForm} from "./Components/Registration Forms/AdvertiserForm";
-import {TourismGovernorForm} from "./Components/Registration Forms/TourismGovernorForm";
-import ExplorePage from "./Pages/ExplorePage";
-import ProfilePage from "./Pages/ProfilePage";
-import Product from "./Pages/ProductsPage";
-import ActivityPage from "./Pages/ActivitiesPage";
-import ActivityForm from "./Components/Model Forms/Activity Form";
-
-
 function App() {
-	const router = createBrowserRouter(
-		createRoutesFromElements(
-			<>
-				<Route path='/' element={<RegisterPage/>}/>
-				<Route path='/tourist' element={<TouristForm/>}/>
-				<Route path='/tourguide' element={<TourGuideForm/>}/>
-				<Route path='/seller' element={<SellerForm/>}/>
-				<Route path='/admin' element={<AdminForm/>}/>
-				<Route path='/advertiser' element={<AdvertiserForm/>}/>
-				<Route path='/tourismgoverner' element={<TourismGovernorForm/>}/>
-				<Route path='/profile' element={<ProfilePage/>}/>
-				<Route path='/search' element={<SearchPage/>}/>
-				<Route path='/activities' element={<ActivityPage/>}/>
-				<Route path='/create-activity' element={<ActivityForm/>}/>
-				<Route path="/explore" element={<ExplorePage/>}/>
-			</>
-		)
-	);
 	return (
-		<RouterProvider router={router}/>
+		<>
+		</>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -4,24 +4,52 @@ import './index.css';
 import App from './App';
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import HomeCards from "./Components/HomeCards";
 import TouristNavBar from "./Components/NavBars/TouristNavBar";
 import AdminNavBar from "./Components/NavBars/AdminNavBar";
+import {BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import RegisterPage from "./Pages/RegisterPage";
+import {TouristForm} from "./Components/Registration Forms/TouristForm";
+import {TourGuideForm} from "./Components/Registration Forms/TourGuideForm";
+import {SellerForm} from "./Components/Registration Forms/SellerForm";
+import {AdminForm} from "./Components/Registration Forms/AdminForm";
+import {TourismGovernorForm} from "./Components/Registration Forms/TourismGovernorForm";
+import ProfilePage from "./Pages/ProfilePage";
+import ActivityPage from "./Pages/ActivitiesPage";
+import ActivityForm from "./Components/Models/Forms/ActivityForm";
+import ExplorePage from "./Pages/ExplorePage";
+import AdvertiserForm from "./Components/Registration Forms/AdvertiserForm";
 import TourGuideNavBar from "./Components/NavBars/TourGuideNavBar";
 import SellerNavBar from "./Components/NavBars/SellerNavBar";
 import AdvertiserNavBar from "./Components/NavBars/AdvertiserNavBar";
 import TourismGovernorNavBar from "./Components/NavBars/TourismGovernorNavBar";
-import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<>
+			<Route path='/' element={<RegisterPage/>}/>
+			<Route path='/tourist' element={<TouristForm/>}/>
+			<Route path='/tourguide' element={<TourGuideForm/>}/>
+			<Route path='/seller' element={<SellerForm/>}/>
+			<Route path='/admin' element={<AdminForm/>}/>
+			<Route path='/advertiser' element={<AdvertiserForm/>}/>
+			<Route path='/tourismgoverner' element={<TourismGovernorForm/>}/>
+			<Route path='/profile' element={<ProfilePage/>}/>
+			<Route path='/activities' element={<ActivityPage/>}/>
+			<Route path='/create-activity' element={<ActivityForm/>}/>
+			<Route path="/explore" element={<ExplorePage/>}/>
+		</>
+	)
+);
+
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-		<NavBarContainer>
-			<App/>
-		</NavBarContainer>
-		<ToastContainer/>
-		</BrowserRouter	>
+		<RouterProvider router={router}>
+			<NavBarContainer>
+				<App/>
+			</NavBarContainer>
+			<ToastContainer/>
+		</RouterProvider>
 	</React.StrictMode>
 );
 
@@ -31,11 +59,15 @@ function NavBarContainer({children}) {
 		setRole(sessionStorage.getItem('role'));
 	}, []);
 	return (
-		<div id='layout'>
+		<>
 			{role}
 			{children}
 			{role === 'tourist' && <TouristNavBar/>}
 			{role === 'admin' && <AdminNavBar/>}
-		</div>
+			{role === 'tour_guide' && <TourGuideNavBar/>}
+			{role === 'seller' && <SellerNavBar/>}
+			{role === 'advertiser' && <AdvertiserNavBar/>}
+			{role === 'tourism_governor' && <TourismGovernorNavBar/>}
+		</>
 	);
 }
