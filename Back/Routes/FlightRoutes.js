fd4d7084b644642c1e557cf5ac51fef99803a1d4
noursaber <nourahmed0803@gmail.com>
2024-11-06 20:55:00 +0200

    flights api

--- Back/Routes/FlightRoutes.js
+++ Back/Routes/FlightRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {searchFlights, getFlightDetails} = require('../Controllers/FlightControllers');
+const {searchFlights, getFlightDetails, bookFlight} = require('../Controllers/FlightControllers');
 
 router.post('/f', searchFlights);
 router.get('/:flightId', getFlightDetails);
+router.post('/book', bookFlight);
 
 module.exports = router;
