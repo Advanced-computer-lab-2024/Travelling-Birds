d3cea37e7cc0fff0a32f21088f28b0ae086bf3f0
noursaber <nourahmed0803@gmail.com>
2024-11-06 19:10:58 +0200

    flights pages

--- Back/Controllers/FlightControllers.js
+++ Back/Controllers/FlightControllers.js
@@ -2,7 +2,6 @@
 const flight = require('../Services/amadeusService');
 let [originGlobal, destinationGlobal, departureDateGlobal] = ["CAI", "JFK", "2025-01-01"];
 
-// Flight Search Controller
 exports.searchFlights = async (req, res) => {
 	const { origin, destination, departureDate } = req.body;
 	originGlobal = origin;
@@ -29,7 +28,6 @@ exports.getFlightDetails = async (req, res) => {
 	const { flightId } = req.params;
 
 	try {
-		// Search for flights using previously stored globals
 		const flightOffersResponse = await flight.shopping.flightOffersSearch.get({
 			originLocationCode: originGlobal,
 			destinationLocationCode: destinationGlobal,
@@ -38,13 +36,11 @@ exports.getFlightDetails = async (req, res) => {
 			currencyCode: "EGP",
 		});
 
-		// Find the specific flight offer that matches flightId
 		const selectedFlight = flightOffersResponse.data.find(flight => flight.id === flightId);
 		if (!selectedFlight) {
 			return res.status(404).json({ message: "Flight not found." });
 		}
 
-		// Perform the pricing step on the selected flight
 		const response = await flight.shopping.flightOffers.pricing.post({
 			data: {
 				type: "flight-offers-pricing",
@@ -54,7 +50,6 @@ exports.getFlightDetails = async (req, res) => {
 			include: "credit-card-fees,detailed-fare-rules"
 		});
 
-		// Send the response back to the client
 		res.json(response.data);
 	} catch (error) {
 		console.log(error);
