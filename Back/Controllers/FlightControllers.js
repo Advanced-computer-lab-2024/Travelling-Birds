3c7121c6200e42524457d6cfd49866cb730f2b63
noursaber <nourahmed0803@gmail.com>
2024-11-06 20:43:16 +0200

    flights api

--- Back/Controllers/FlightControllers.js
+++ Back/Controllers/FlightControllers.js
@@ -56,3 +56,73 @@ exports.getFlightDetails = async (req, res) => {
 		res.status(500).json({ message: error.description });
 	}
 };
+
+// Book Flight Controller
+exports.bookFlight = async (req, res) => {
+	const { flightId, travelerDetails } = req.body;
+	try {
+		const flightOffersResponse = await flight.shopping.flightOffersSearch.get({
+			originLocationCode: originGlobal,
+			destinationLocationCode: destinationGlobal,
+			departureDate: departureDateGlobal,
+			adults: "1",
+			currencyCode: "EGP",
+		});
+
+		const selectedFlight = flightOffersResponse.data.find(flight => flight.id === flightId);
+		if (!selectedFlight) {
+			return res.status(404).json({ message: "Flight not found." });
+		}
+
+		const pricingResponse = await flight.shopping.flightOffers.pricing.post({
+			data: {
+				type: "flight-offers-pricing",
+				flightOffers: [selectedFlight],
+			},
+		});
+
+		const bookingResponse = await flight.booking.flightOrders.post({
+			data: {
+				type: "flight-order",
+				flightOffers: pricingResponse.data.flightOffers,
+				travelers: travelerDetails.map((traveler, index) => ({
+					id: (index + 1).toString(),
+					dateOfBirth: traveler.dateOfBirth,
+					name: {
+						firstName: traveler.firstName,
+						lastName: traveler.lastName,
+					},
+					gender: traveler.gender,
+					contact: {
+						emailAddress: traveler.emailAddress,
+						phones: [
+							{
+								deviceType: "MOBILE",
+								countryCallingCode: "20",  // Replace with the actual country calling code
+								number: traveler.phone,
+							},
+						],
+					},
+					documents: [
+						{
+							documentType: "PASSPORT",
+							number: traveler.passportNumber,
+							expiryDate: traveler.passportExpiry,
+							issuanceCountry: traveler.issuanceCountry,
+							nationality: traveler.nationality,
+						},
+					],
+				})),
+			},
+		});
+
+		res.status(200).json({
+			message: "Booking confirmed!",
+			bookingDetails: bookingResponse.data,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.description || "An error occurred while booking the flight." });
+	}
+};
+
